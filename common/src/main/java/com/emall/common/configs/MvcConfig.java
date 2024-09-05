6960c940f2bb91f3cede73f1be4a874c29db3628
Zincoid <2660181154@qq.com>
2024-09-06 00:55:23 +0800

    E-Mall

--- common/src/main/java/com/emall/common/configs/MvcConfig.java
+++ common/src/main/java/com/emall/common/configs/MvcConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-// @ConditionalOnProperty(name = "UserInfoInterceptor.enable", havingValue = "true")
+@ConditionalOnProperty(name = "UserInfoInterceptor.enable", havingValue = "true")
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
