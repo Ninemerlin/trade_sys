7b69d7f38529174fe8c4a4147c6fda7b7fa9ac72
Zincoid <2660181154@qq.com>
2024-09-06 01:21:33 +0800

    E-Mall

--- common/src/main/java/com/emall/common/configs/MvcConfig.java
+++ common/src/main/java/com/emall/common/configs/MvcConfig.java
@@ -9,6 +9,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @ConditionalOnProperty(name = "UserInfoInterceptor.enable", havingValue = "true")
 public class MvcConfig implements WebMvcConfigurer {
+//     微服务全局拦截器使用:
+//     1.引入common依赖
+//     <dependency>
+//          <groupId>com.emall</groupId>
+//          <artifactId>common</artifactId>
+//          <version>0.0.1-SNAPSHOT</version>
+//          <scope>compile</scope>
+//     </dependency>
+//     2.yaml添加配置
+//     UserInfoInterceptor:
+//         enable: true
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new UserInfoInterceptor());
