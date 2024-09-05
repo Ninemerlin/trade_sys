0efac742713e39a2b4f6ad9691c66b1afeb6d74f
Zincoid <2660181154@qq.com>
2024-09-05 09:05:31 +0800

    E-Mall

---
+++ user-service/src/test/java/com/emall/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emall.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
