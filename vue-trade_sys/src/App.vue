8040f2a6f9e57aad899611229d6d6c255cf4716a
You88ng555 <2024489117@qq.com>
2024-09-19 14:37:10 +0800

    scss文件自动导入

--- vue-trade_sys/src/App.vue
+++ vue-trade_sys/src/App.vue
@@ -6,4 +6,13 @@
   <!-- <el-button type="primary">Primary</el-button> -->
   <!-- 一级路由出口组件 -->
   <RouterView />
+  <div class="test">
+    test scss
+  </div>
 </template>
+
+<style scoped lang="scss">
+.test{
+  color: $priceColor;
+}
+</style>
